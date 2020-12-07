b6a7e7b2f9acbf79f58f3cecd9338286b8e7552b
moink <theresa.robinson@gmail.com>
2020-12-07 07:01:18 +0100

    cleaned up day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,16 +1,19 @@
 import collections
 
-import advent_tools
+
+def run_part_1(lines):
+    rules = collections.defaultdict(list)
+    for line in lines:
+        left, right = line.split(' contain ')
+        holder = clean_colour(left)
+        for contents in right.split(','):
+            contained = clean_colour(contents[2:])
+            rules[contained].append(holder)
+    return len(list_all_holders(rules, 'shiny gold'))
 
 
-def process_input(data):
-    result = collections.defaultdict(list)
-    for key, val in data.items():
-        for contents in val.split(','):
-            colour = contents[2:].replace(' bags','').replace(' bag', '').replace('.', '').strip()
-            holder = key.replace(' bags','').replace(' bag', '').replace('.', '').strip()
-            result[colour].append(holder)
-    return result
+def clean_colour(colour):
+    return colour.replace(' bags', '').replace(' bag', '').replace('.', '').strip()
 
 
 def list_all_holders(data, colour):
@@ -20,15 +23,15 @@ def list_all_holders(data, colour):
     return result
 
 
-def run_part_1():
-    data = advent_tools.read_dict_from_input_file(sep=' contain ', key='left')
+def run_part_2(lines):
     rules = collections.defaultdict(list)
-    for key, val in data.items():
-        holder = key.replace(' bags', '').replace(' bag', '').replace('.', '').strip()
-        for contents in val.split(','):
-            colour = contents[2:].replace(' bags', '').replace(' bag', '').replace('.', '').strip()
-            rules[colour].append(holder)
-    return len(list_all_holders(rules, 'shiny gold'))
+    for line in lines:
+        left, right = line.split(' contain ')
+        holder = clean_colour(left)
+        for contents in right.split(','):
+            contained = clean_colour(contents)
+            rules[holder].append(contained)
+    return count_contents(rules, 'shiny gold')
 
 
 def count_contents(data, colour):
@@ -36,27 +39,18 @@ def count_contents(data, colour):
         return 0
     count = 0
     for cont in data[colour]:
-        numstr = cont.split()[0]
-        if numstr == 'no':
+        num_str = cont.split()[0]
+        if num_str == 'no':
             num = 0
         else:
-            num = int(numstr)
+            num = int(num_str)
         col = cont.split(maxsplit=1)[1]
         count = count + num * (count_contents(data, col) + 1)
     return count
 
 
-def run_part_2():
-    data = advent_tools.read_dict_from_input_file(sep=' contain ', key='right')
-    result = collections.defaultdict(list)
-    for key, val in data.items():
-        colour = val.replace(' bags', '').replace(' bag', '').replace('.', '').strip()
-        for contents in key.split(','):
-            holder = contents.replace(' bags', '').replace(' bag', '').replace('.', '').strip()
-            result[colour].append(holder)
-    return count_contents(result, 'shiny gold')
-
-
 if __name__ == '__main__':
-    print('Part 1:', run_part_1())
-    print('Part 2:', run_part_2())
+    with open('input.txt') as in_file:
+        input_lines = in_file.read().strip().splitlines()
+    print('Part 1:', run_part_1(input_lines))
+    print('Part 2:', run_part_2(input_lines))
