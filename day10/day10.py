b0ea7f85aef103f4d8bb0c276b89f17958f4c1a9
moink <theresa.robinson@gmail.com>
2020-12-10 06:49:50 +0100

    tiny bit of cleanup

--- day10/day10.py
+++ day10/day10.py
@@ -4,10 +4,6 @@ import functools
 import advent_tools
 
 
-def process_input(data):
-    return sorted(data)
-
-
 def run_part_1(data):
     diffs = [a - b for a, b in zip(data[1:], data[:-1])]
     count = collections.Counter(diffs)
@@ -18,10 +14,8 @@ def run_part_1(data):
 def count_ways(data, start, end):
     if start == end:
         return 1
-    next_nums = [num for num in data if 1 <= num - start <=3]
-    count = 0
-    for next_num in next_nums:
-        count = count + count_ways(data, next_num, end)
+    next_nums = [num for num in data if 1 <= num - start <= 3]
+    count = sum(count_ways(data, next_num, end) for next_num in next_nums)
     return count
 
 
@@ -31,7 +25,6 @@ def run_part_2(data):
 
 
 if __name__ == '__main__':
-    data = advent_tools.read_one_int_per_line()
-    data = process_input(data)
-    print('Part 1:', run_part_1(data))
-    print('Part 2:', run_part_2(data))
+    input_data = sorted(advent_tools.read_one_int_per_line())
+    print('Part 1:', run_part_1(input_data))
+    print('Part 2:', run_part_2(input_data))
