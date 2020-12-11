966c14f3aa594feb43fbced511d68de51afb0b48
moink <theresa.robinson@gmail.com>
2020-12-11 07:04:54 +0100

    day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -30,18 +30,42 @@ class GameOfLife(advent_tools.PlottingGrid):
     # convolve_matrix = np.asarray([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
     walls_treated_as = 0
 
-    def count_neighbours(self):
+    def count_neighbours_part_one(self):
         """Count the number of neighbours each grid point has"""
         count = signal.convolve2d(self.grid == 1, self.convolve_matrix,
                                   mode='same', boundary='fill',
                                   fillvalue=self.walls_treated_as)
-        # plt.imshow(count)
-        # plt.show()
         return count
+
+    def count_neighbours_part_two(self):
+        """Count the number of neighbours each grid point has"""
+        directions = [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)]
+        count = np.zeros(self.grid.shape)
+        for (y, x), _ in np.ndenumerate(self.grid):
+            for dir in directions:
+                count[y, x] = count[y, x] + self.next_neighbour(y, x, dir)
+        return count
+
+    def next_neighbour(self, y, x, dir):
+        del_y, del_x = dir
+        while True:
+            y = y + del_y
+            x = x + del_x
+            if y >= 0 and x >= 0:
+                try:
+                    val = self.grid[y, x]
+                except IndexError:
+                    return 0
+                if val < 2:
+                    return val
+            else:
+                return 0
+
+
 
     def one_step(self):
         """Take one game of life step"""
-        counts = self.count_neighbours()
+        counts = self.count_neighbours_part_two()
         self.evaluate_where_on(counts)
         self.draw()
 
@@ -59,7 +83,7 @@ class GameOfLife(advent_tools.PlottingGrid):
         # Otherwise turn off
         # old_grid = copy.deepcopy(self.grid)
         self.grid = np.where(np.logical_and(self.grid == 1,
-                                            counts >= 4), 0, self.grid)
+                                            counts >= 5), 0, self.grid)
         # For grid points that are off, if count is 3, turn on
         # Otherwise keep the same as grid calculated in previous step
         self.grid = np.where(np.logical_and(self.grid == 0, counts == 0), 1,
@@ -86,6 +110,9 @@ class GameOfLife(advent_tools.PlottingGrid):
     def count_ones(self):
         return sum(sum(self.grid == 1))
 
+
+
+
 def process_input(data):
     print(data)
     return data
@@ -97,20 +124,12 @@ def run_part_1():
     return g.count_ones()
 
 
-def run_part_2(data):
-    pass
+def run_part_2():
+    g = GameOfLife.from_file({'L': 0, '#': 1, '.': 2})
+    g.simulate_n_steps()
+    return g.count_ones()
 
 
 if __name__ == '__main__':
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
     print('Part 1:', run_part_1())
-    # print('Part 2:', run_part_2(data))
+    print('Part 2:', run_part_2())
