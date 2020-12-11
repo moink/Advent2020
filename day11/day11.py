f9893a8946eeff33fc77a65668c5054ac2588e44
moink <theresa.robinson@gmail.com>
2020-12-11 07:36:34 +0100

    refactor day 11

--- day11/day11.py
+++ day11/day11.py
@@ -1,19 +1,25 @@
 import copy
-import functools
-import itertools
-import math
-from matplotlib import pyplot as plt
 import numpy as np
-import pandas as pd
-import re
 from scipy import signal
 
 import advent_tools
 
 
-class GameOfLife(advent_tools.PlottingGrid):
+class SeatingSystem(advent_tools.PlottingGrid):
     convolve_matrix = np.asarray([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
     walls_treated_as = 0
+
+    @classmethod
+    def seating_from_file(cls, char_map, part, plotting):
+        new_grid = super().from_file(char_map)
+        if part == 1:
+            new_grid.count_fun = cls.count_neighbours_part_one
+            new_grid.limit = 4
+        else:
+            new_grid.count_fun = cls.count_neighbours_part_two
+            new_grid.limit = 5
+        new_grid.plotting = plotting
+        return new_grid
 
     def count_neighbours_part_one(self):
         """Count the number of neighbours each grid point has for part 1"""
@@ -24,7 +30,8 @@ class GameOfLife(advent_tools.PlottingGrid):
 
     def count_neighbours_part_two(self):
         """Count the number of neighbours each grid point has for part 2"""
-        directions = [(0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1)]
+        directions = [(0, 1), (-1, 1), (-1, 0), (-1, -1),
+                      (0, -1), (1, -1), (1, 0), (1, 1)]
         count = np.zeros(self.grid.shape)
         for (y, x), _ in np.ndenumerate(self.grid):
             for dir in directions:
@@ -46,72 +53,36 @@ class GameOfLife(advent_tools.PlottingGrid):
             else:
                 return 0
 
-    def one_step(self, count_fun, limit):
-        """Take one game of life step"""
-        counts = count_fun()
-        self.evaluate_where_on(counts, limit)
-        self.draw()
+    def one_step(self):
+        counts = self.count_fun(self)
+        self.evaluate_where_on(counts)
+        if self.plotting:
+            self.draw()
 
-    def evaluate_where_on(self, counts, limit):
-        """Set the grid to the right setting based on the neighbour count
-
-        Args:
-            counts: np.ndarray
-                Number of neighbours of each node that are on
-
-        Returns:
-            None
-        """
-        # For grid points that are on, if count is 2 or 3, keep on
-        # Otherwise turn off
-        # old_grid = copy.deepcopy(self.grid)
+    def evaluate_where_on(self, counts):
         self.grid = np.where(np.logical_and(self.grid == 1,
-                                            counts >= limit), 0, self.grid)
-        # For grid points that are off, if count is 3, turn on
-        # Otherwise keep the same as grid calculated in previous step
+                                            counts >= self.limit), 0, self.grid)
         self.grid = np.where(np.logical_and(self.grid == 0, counts == 0), 1,
                              self.grid)
-        # print('')
 
-    def simulate_n_steps(self, count_fun, limit):
-        """Take a defined number of steps of the Game of Life
-
-        Args:
-            num_steps: int
-                Number of steps to take
-
-        Returns:
-            None
-        """
+    def run_until_stable(self):
         while True:
             old_grid = copy.deepcopy(self.grid)
-            self.one_step(count_fun, limit)
+            self.one_step()
             if (old_grid == self.grid).all().all():
                 break
-        self.draw()
+        if self.plotting:
+            self.show()
 
     def count_ones(self):
         return sum(sum(self.grid == 1))
 
 
-
-
-def process_input(data):
-    print(data)
-    return data
-
-def run_part_1():
-    g = GameOfLife.from_file({'L': 0, '#': 1, '.':2})
-    g.simulate_n_steps(g.count_neighbours_part_one, 4)
+def run_part(part_num):
+    g = SeatingSystem.seating_from_file({'L': 0, '#': 1, '.': 2}, part_num, False)
+    g.run_until_stable()
     return g.count_ones()
-
-
-def run_part_2():
-    g = GameOfLife.from_file({'L': 0, '#': 1, '.': 2})
-    g.simulate_n_steps(g.count_neighbours_part_two, 5)
-    return g.count_ones()
-
 
 if __name__ == '__main__':
-    print('Part 1:', run_part_1())
-    print('Part 2:', run_part_2())
+    print('Part 1:', run_part(1))
+    print('Part 2:', run_part(2))
