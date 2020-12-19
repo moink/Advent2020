d032405c2f1b5e32d004e1c0a83f13119d55e591
moink <theresa.robinson@gmail.com>
2020-12-19 10:30:54 +0100

    refactor day 19

--- day19/day19.py
+++ day19/day19.py
@@ -4,6 +4,7 @@ import advent_tools
 
 
 def main():
+    # advent_tools.TESTING = True
     data = advent_tools.read_input_line_groups()
     print('Part 1:', run_part_1(data[0], data[1]))
     print('Part 2:', run_part_2(data[0], data[1]))
@@ -24,30 +25,33 @@ def process_rules(data):
 
 def run_part_1(rules, messages):
     rules = process_rules(rules)
-    return sum('' in check_rule(message, 0, rules) for message in messages)
+    return sum(check_rule(message, rules) for message in messages)
 
 
 def run_part_2(rules, messages):
     rules = rules + ['8: 42 | 42 8', '11: 42 31 | 42 11 31']
     rules = process_rules(rules)
-    return sum('' in check_rule(message, 0, rules) for message in messages)
+    return sum(check_rule(message, rules) for message in messages)
 
 
-def check_rule(message, rule_num, rules):
-    for option in rules[rule_num]:
-        if isinstance(option, str):
-            if message.startswith(option):
-                yield message[1:]
-            continue
-        rems = (message,)
-        for num in option:
-            all_rems = []
+def check_rule(message, rules):
+    return '' in calc_remainders(message, 0, rules)
+
+
+def calc_remainders(message, rule_num, rules):
+    cur_rule = rules[rule_num]
+    if len(cur_rule) == 1 and isinstance(cur_rule[0], str):
+        if message.startswith(cur_rule[0]):
+            yield message[1:]
+    else:
+        for option in cur_rule:
+            rems = [message]
+            for num in option:
+                next_rems = [r for rem in rems
+                             for r in calc_remainders(rem, num, rules)]
+                rems = next_rems
             for rem in rems:
-                for r in check_rule(rem, num, rules):
-                    all_rems.append(r)
-            rems = tuple(all_rems)
-        for rem in rems:
-            yield rem
+                yield rem
 
 
 if __name__ == '__main__':
